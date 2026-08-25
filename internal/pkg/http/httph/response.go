5b0223d422c08c7b5f42ee77056dd9fd3f88ef31
kd <kdarenskii@mail.ru>
2026-08-25 17:07:00 +0300

    TASK-1005: добавил сервисный слой и обработчики

---
+++ internal/pkg/http/httph/response.go
@@ -0,0 +1,56 @@
+package httph
+
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+
+	"github.com/gofrs/uuid"
+	"github.com/gorilla/mux"
+
+	"github.com/KDarenskii/catalog-service/internal/app/entity"
+)
+
+type httpCoder interface {
+	error
+	HTTPStatus() int
+}
+
+func SendJSON(w http.ResponseWriter, status int, data interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	_ = json.NewEncoder(w).Encode(data)
+}
+
+func SendEmpty(w http.ResponseWriter, status int) {
+	w.WriteHeader(status)
+}
+
+func sendError(w http.ResponseWriter, status int, err error) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	_ = json.NewEncoder(w).Encode(map[string]string{"error": err.Error()})
+}
+
+func HandleError(w http.ResponseWriter, err error) {
+	var hc httpCoder
+	if errors.As(err, &hc) {
+		sendError(w, hc.HTTPStatus(), hc)
+		return
+	}
+	sendError(w, http.StatusInternalServerError, err)
+}
+
+func ParseParam[T any](r *http.Request, name string, parse func(string) (T, error)) (T, error) {
+	raw := mux.Vars(r)[name]
+	value, err := parse(raw)
+	if err != nil {
+		var zero T
+		return zero, entity.ErrIncorrectParameters
+	}
+	return value, nil
+}
+
+func ParseUUIDParam(r *http.Request, name string) (uuid.UUID, error) {
+	return ParseParam(r, name, uuid.FromString)
+}
