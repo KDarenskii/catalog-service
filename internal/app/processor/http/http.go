882568d9116ce27eee036903d4883cc403af81da
kd <kdarenskii@mail.ru>
2026-09-06 13:39:50 +0300

    TASK-2004: исправил передачу контекста

--- internal/app/processor/http/http.go
+++ internal/app/processor/http/http.go
@@ -76,7 +76,7 @@ func (p *httpProc) StartAsync(ctx context.Context, wg *sync.WaitGroup) {
 
 	processor.WatchForShutdown(ctx, wg, processor.CloserFunc(l.Close))
 
-	processor.WatchForShutdown(ctx, wg, processor.NewCloserContextFunc(p.server.Shutdown, ctx, 5*time.Second))
+	processor.WatchForShutdown(ctx, wg, processor.NewCloserContextFunc(p.server.Shutdown, context.Background(), 5*time.Second))
 }
 
 func (p *httpProc) serve(l net.Listener) {
