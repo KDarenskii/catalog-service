34adb5a8c42596947a8930cdba3da837438bb3dc
kd <kdarenskii@mail.ru>
2026-09-23 17:46:54 +0300

    TASK-4005: добавил http endpoints для профилирования

--- internal/app/processor/http/api_generic.go
+++ internal/app/processor/http/api_generic.go
@@ -2,6 +2,7 @@ package rprocessor
 
 import (
 	"net/http"
+	"net/http/pprof"
 
 	"github.com/gorilla/mux"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
@@ -16,4 +17,26 @@ func vGenericRegHealthCheck(r *mux.Router, h rhandler.Health) {
 
 func handlerNotFound(w http.ResponseWriter, _ *http.Request) {
 	w.WriteHeader(http.StatusNotFound)
+}
+
+func vGenericRegPprof(r *mux.Router) {
+	reg(r, http.MethodGet, "/debug/pprof", http.HandlerFunc(pprofIndex))
+	reg(r, http.MethodGet, "/debug/pprof/", http.HandlerFunc(pprof.Index))
+
+	reg(r, http.MethodGet, "/debug/pprof/cmdline", http.HandlerFunc(pprof.Cmdline))
+	reg(r, http.MethodGet, "/debug/pprof/profile", http.HandlerFunc(pprof.Profile))
+	reg(r, http.MethodGet, "/debug/pprof/trace", http.HandlerFunc(pprof.Trace))
+
+	r.Methods(http.MethodGet, http.MethodPost).Path("/debug/pprof/symbol").Handler(http.HandlerFunc(pprof.Symbol))
+
+	reg(r, http.MethodGet, "/debug/pprof/allocs", pprof.Handler("allocs"))
+	reg(r, http.MethodGet, "/debug/pprof/block", pprof.Handler("block"))
+	reg(r, http.MethodGet, "/debug/pprof/goroutine", pprof.Handler("goroutine"))
+	reg(r, http.MethodGet, "/debug/pprof/heap", pprof.Handler("heap"))
+	reg(r, http.MethodGet, "/debug/pprof/mutex", pprof.Handler("mutex"))
+	reg(r, http.MethodGet, "/debug/pprof/threadcreate", pprof.Handler("threadcreate"))
+}
+
+func pprofIndex(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "/debug/pprof/", http.StatusMovedPermanently)
 }
